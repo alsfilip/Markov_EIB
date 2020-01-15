b57227913897062d72e90faa0b5ac9ab73ef13f4
Alexandre Filipowicz <alsfilip@gmail.com>
2020-01-15 16:41:16 -0500

    Updated p_joint in embo -> utils

--- embo/embo/utils.py
+++ embo/embo/utils.py
@@ -23,9 +23,12 @@ def p_joint(x1,x2,windowx1=1,windowx2=1):
     aux_base_x1 = numuniquex1**np.arange(windowx1)[::-1]
     aux_base_x2 = numuniquex2**np.arange(windowx2)[::-1]
     px1x2 = np.zeros((numwordsx1,numwordsx2)) #matrix of size numwordsx,numwordsy with for the joint probability distribution
-    for i in range(len(x1)-windowx1):
-        x1i = np.inner(x1[i:i+windowx1], aux_base_x1).astype(np.int)
-        x2i = np.inner(x2[i:i+windowx2], aux_base_x2).astype(np.int)
+#     for i in range(len(x1)-windowx1):
+    for i in range(len(x1)):
+        x1i = np.inner(x1[i], aux_base_x1).astype(np.int)
+        x2i = np.inner(x2[i], aux_base_x2).astype(np.int)
+#         x1i = np.inner(x1[i:i+windowx1], aux_base_x1).astype(np.int)
+#         x2i = np.inner(x2[i:i+windowx2], aux_base_x2).astype(np.int)
         px1x2[x1i,x2i] += 1
     return px1x2/px1x2.sum()
 
